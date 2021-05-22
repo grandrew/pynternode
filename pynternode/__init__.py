4c9ab0a29d3872b3f2847c75dcc86fdd5eaa80ee
Andrew Gree <realgrandrew@gmail.com>
2021-05-22 15:40:10 -0700

    fixes for main ops

--- pynternode/__init__.py
+++ pynternode/__init__.py
@@ -21,23 +21,44 @@ class NodeJSRepl:
     def _wait_finish(self):
         self.nodejs.expect("async>")
     
-    def run(self, cmdline):
+    def execl(self, cmdline):
+        print("Sending line to nodejs:", cmdline)
         self.nodejs.sendline(cmdline)
+    
+    def run(self, cmdline):
+        self.execl(cmdline)
         self._wait_finish()
     
-    def ret_str(self, cmdline):
-        self.nodejs.sendline(cmdline)
+    def ret_raw(self, cmdline):
+        self.execl(cmdline)
         out = self.nodejs.readline()
         out = self.nodejs.readline()
         out = ansi_escape_8bit.sub(b'', out).strip()
         self._wait_finish()
+        print(out)
         return out
 
     def ret_int(self, cmdline):
-        return int(self.ret_str(cmdline))
+        out = "<incomplete>"
+        try:
+            out = self.ret_raw(cmdline)
+            return int(out)
+        except:
+            raise ValueError(f"Could not parse result {out}")
+
+    def ret_float(self, cmdline):
+        return float(self.ret_raw(cmdline))
+    
+    def ret_int_cast(self, cmdline):
+        return int(self.ret_float(cmdline))
+
+    def ret_str(self, cmdline):
+        out = self.ret_raw(cmdline=cmdline).decode("utf-8")
+        assert out.startswith("'") and out.endswith("'"), f"Output does not seem to be a string: `{out}`"
+        return out[1:-1]
     
     def ret_json(self, cmdline):
-        return json.loads(self.ret_str(cmdline)[1:-1])
+        return json.loads(self.ret_raw(cmdline)[1:-1])
     
     def __del__(self):
         self.nodejs.sendline("process.exit()")
