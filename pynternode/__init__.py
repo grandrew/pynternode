0709b476b255f88f285eacce1487890b1855956d
Andrew Gree <realgrandrew@gmail.com>
2021-05-24 18:09:07 -0700

    WIP delays and error handling

--- pynternode/__init__.py
+++ pynternode/__init__.py
@@ -1,6 +1,7 @@
 import pexpect
 import json
 import re
+import time
 
 # 7-bit and 8-bit C1 ANSI sequences
 ansi_escape_8bit = re.compile(
@@ -18,25 +19,101 @@ class NodeJSRepl:
             traceback.print_exc()
             raise RuntimeError(f"Please make sure async-repl is installed in {async_repl_executable}")
     
+    def _wait_finish_strict(self):
+        index = self.nodejs.expect_exact(["Thrown:", "async>"])
+        if index == 0:
+            raise RuntimeError("Exception in JS")
+        while True:
+            try:
+                out = self.nodejs.read_nonblocking(timeout=0.5)
+            except:
+                break 
+
     def _wait_finish(self):
-        self.nodejs.expect("async>")
+        print("WAIT FINISH")
+        out = b""
+        ts = time.time()
+        while True:
+            try:
+                out += self.nodejs.read_nonblocking(timeout=0.5)
+                if b"async>" in out:
+                    break
+            except:
+                time.sleep(0.1) 
+                if time.time() - ts > 1:
+                    print("WAIT IFNISH TIMEIOUYT")
+                    break
+        out = ansi_escape_8bit.sub(b'', out).strip().decode("utf-8")
+        if "Thrown:" in out:
+            raise RuntimeError(f"Exception in JS: {out}")
+        print("WAIT FINISH OUT IS:\n", out)
+        time.sleep(0.05)
+        return out
+    
+    def exec_logall(self, cmdline):
+        print("=====================")
+        print("EXEC_LOGALL Sending line to nodejs:", cmdline)
+        self.nodejs.sendline("")
+        time.sleep(0.05)
+        self._wait_finish()
+        time.sleep(0.05)
+        self.nodejs.sendline(cmdline)
+        out = b""
+        while True:
+            try:
+                out += self.nodejs.read_nonblocking(timeout=0.5)
+            except:
+                break
+        out = ansi_escape_8bit.sub(b'', out).strip()
+        print(out.decode("utf-8"))
     
     def execl(self, cmdline):
+        print("=====================")
         print("Sending line to nodejs:", cmdline)
-        self.nodejs.sendline(cmdline)
+        self.nodejs.sendline("")
+        time.sleep(0.05)
+        self._wait_finish()
+        self.nodejs.sendline("")
+        time.sleep(0.05)
+        self._wait_finish()
+        self.nodejs.sendline("")
+        time.sleep(0.05)
+        self._wait_finish()
+        time.sleep(0.05)
+        self.nodejs.sendline(cmdline.encode("utf-8"))
+        time.sleep(0.2)
+        # out = self.nodejs.readline()
+        # out = ansi_escape_8bit.sub(b'', out).strip()
+        # print(out)
     
     def run(self, cmdline):
+        print("Running no result")
         self.execl(cmdline)
         self._wait_finish()
+        self.nodejs.sendline("")
+        time.sleep(0.05)
+        self._wait_finish()
+        self.nodejs.sendline("")
+        time.sleep(0.05)
+        self._wait_finish()
+        self.nodejs.sendline("")
+        time.sleep(0.05)
+        self._wait_finish()
+        print("END Running no result")
     
     def ret_raw(self, cmdline):
         self.execl(cmdline)
         out = self.nodejs.readline()
-        out = self.nodejs.readline()
-        out = ansi_escape_8bit.sub(b'', out).strip()
+        while out.startswith(b" ") or out.startswith(b'(node:') or b"async>" in out:
+            print("Raw ret (wait for more)", out)
+            out = self.nodejs.readline()
+        if cmdline in out.decode("utf-8"):
+            out = self.nodejs.readline()
+        out_fix = ansi_escape_8bit.sub(b'', out).strip()
+        print("Final return(next is wait finish)>>>", out_fix)
         self._wait_finish()
-        print(out)
-        return out
+        print("Final return>>>", out)
+        return out_fix
 
     def ret_int(self, cmdline):
         out = "<incomplete>"
